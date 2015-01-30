5251bd15362d1753c68e43cc48a583c3c47b45f2
yangq79@gmail.com <yangq79@gmail.com>
2015-01-29 20:45:10 -0500

    initial push

---
+++ 3Dogs1Movie/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace _3Dogs1Movie
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
